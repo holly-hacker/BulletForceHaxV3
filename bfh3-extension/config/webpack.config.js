d981adab5ad17fc30403ce0359f07d5d4bbaa0b2
HoLLy <dario.db@hotmail.com>
2025-02-13 18:15:04 +0100

    Apply new editorconfig style to generated extension

--- bfh3-extension/config/webpack.config.js
+++ bfh3-extension/config/webpack.config.js
@@ -7,13 +7,13 @@ const PATHS = require('./paths');
 
 // Merge webpack configuration files
 const config = (env, argv) =>
-  merge(common, {
-    entry: {
-      popup: PATHS.src + '/popup.js',
-      contentScript: PATHS.src + '/contentScript.js',
-      background: PATHS.src + '/background.js',
-    },
-    devtool: argv.mode === 'production' ? false : 'source-map',
-  });
+	merge(common, {
+		entry: {
+			popup: PATHS.src + '/popup.js',
+			contentScript: PATHS.src + '/contentScript.js',
+			background: PATHS.src + '/background.js',
+		},
+		devtool: argv.mode === 'production' ? false : 'source-map',
+	});
 
 module.exports = config;
---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in with pkgs; rec {
+        devShells.default = mkShell rec {
+          nativeBuildInputs = [
+            nodejs_22
+          ];
+          buildInputs = [];
+          packages = [];
+        };
+      });
+}
