719bdfe1d08568fa978fb7fc216fd3be6851557d
HoLLy <dario.db@hotmail.com>
2025-03-02 13:47:19 +0100

    Move top-level react code into components

---
+++ bfh3-extension/src/devtools/components/DevtoolsTab.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import TopPanel from "./TopPanel";
+import MessageListWithSidePanel from "./MessageListWithSidePanel";
+
+export default () => {
+	const topTabHeight = 48;
+
+	return (
+		<div style={{ height: '100vh' }}>
+			<div style={{ height: `${topTabHeight}px`, padding: '4px' }}>
+				<TopPanel />
+			</div>
+			<div style={{ height: `calc(100% - ${topTabHeight}px)`, overflow: 'auto', }}>
+				<MessageListWithSidePanel />
+			</div>
+		</div>
+	);
+}
--- bfh3-extension/src/devtools/panel.tsx
+++ bfh3-extension/src/devtools/panel.tsx
@@ -1,28 +1,15 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
-import MessageListWithSidePanel from './components/MessageListWithSidePanel';
+import DevtoolsTab from './components/DevtoolsTab';
 
 const rootNode = document.getElementById('root');
 if (!rootNode) throw "no root node found";
 
 const root = createRoot(rootNode);
 
-const App = () => {
-	return (
-		<div style={{ height: '100vh' }}>
-			<div style={{ height: '48px', padding: '4px' }}>
-				Hello devtools!
-			</div>
-			<div style={{ height: 'calc(100% - 48px)', overflow: 'auto', }}>
-				<MessageListWithSidePanel />
-			</div>
-		</div>
-	);
-}
-
 root.render((
 	<StrictMode>
-		<App></App>
+		<DevtoolsTab />
 	</StrictMode>
 ));
